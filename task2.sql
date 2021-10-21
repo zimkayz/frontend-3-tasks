109f8108f392c1d868e1deb31b1da79914446ac8
Elijah <kutsiwaelijah@gmail.com>
2021-10-21 12:34:46 +0300

    Add files via upload
    Тестовое задание по HTML/CSS
    
    Требуется сверстать интерфейс чата, который представляет собой окно разделённое на 4 области: шапка с фиксированной высотой 50 пикселей, подвал с фиксированной высотой 100 пикселей, левая панель с фиксированной шириной 200 пикселей, правая панель. Чат занимает 100% ширины и 100% высоты окна, внутри чат делится на зоны как на картинке. Шапка (1) предназначена для элементов управления и имеет фиксированную высоту, подвал (4) предназначен для ввода текста и имеет фиксированную высоту, левая панель (2) предназначена для вывода списка участников чата и может иметь внутреннюю прокрутку, правая панель (3) предназначена для отображения сообщений в чате и может иметь внутреннюю прокрутку.
    
    Для демонстрации работоспособности вёрстки нужно в левую (2) и правую (3) панель вставить достаточно контента, чтобы появилась полоса прокрутки. Проследить за тем, чтобы не появилась общая полоса прокрутки для всего чата, проследить за тем, чтобы подвал и шапка занимали строго фиксированную высоту. Контент внутри четырёх зон выравнивается по левому краю и прижимается к верху контейнера.
    
    Вёрстка должна работать в браузерах Internet Explorer, Google Chrome, Mozilla Firefox, Apple Safari не старше 2 лет. Решение заадания прислать на почту support@plarson.ru с темой «Вакансия верстальщика». JavaScript не использовать.
    
    В более сложном варианте сделать так, чтобы шапка и подвал могли растягиваться под контент (т.е. есть только min-height), а центральная часть это учитывала.
    
    Код CSS должен быть необходимым и достаточным (не содержать лишних сущностей), разметка кода и комментарии имеют значение.
    https://plarson.ru/tn1000x0/i/test800x605.png
    
    Тестовое задание по SQL(MySQL)
    Составить запрос, который будет выдавать результат в виде таблицы (а не строки!) из 12 строк, в каждой строке 2 колонки, в одной колонке месяц текущего года, в другой — количество дней в месяце. В запросе не использовать существующие таблицы из БД, в том числе временные, т.е. запрос должен работать сразу из любой консоли sql.
    
    Тестовое задание по JavaScript
    Реализовать функционал на чистом JavaScript, аналогичный работе события window.onload, конечным итогом которого будет alert или console.log с текстом о том, что страница полностью загружена.

---
+++ task2.sql
@@ -0,0 +1,15 @@
+SELECT 'March', 31
+UNION
+SELECT 'April', 30
+UNION
+SELECT 'May', 31
+UNION
+SELECT 'june', 30
+UNION
+SELECT 'July', 31
+UNION
+SELECT 'August', 30
+UNION
+SELECT 'September', 31
+
+
